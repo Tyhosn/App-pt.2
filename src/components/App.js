9bd819bfda4b087561ad380daa2a67ded2b7b1b2
Tyhosn <105897683+Tyhosn@users.noreply.github.com>
2023-06-03 00:24:17 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map((noted) => (
+        <Note key={noted.key} title={noted.title} content={noted.content} />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
